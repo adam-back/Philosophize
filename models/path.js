6a2b43fa184d95b6f2ac1aa0e1c92c4f1713d5c8
Adam Back <adb33@email.arizona.edu>
2016-07-29 17:28:22 -0700

    (feat) Added hooks to aid in automatic denormalization.

--- models/path.js
+++ models/path.js
@@ -20,5 +20,16 @@ module.exports = function( sequelize, DataTypes ) {
     }
   }, { underscored: true } );
 
+  path.beforeCreate(function( newPath ) {
+    newPath.number_of_steps = newPath.order_of_pages.length;
+    if ( newPath.number_of_steps ) {
+      newPath.start_page_id = newPath.order_of_pages[ 0 ];
+    }
+  });
+
+  path.beforeUpdate(function( updatedPath ) {
+    updatedPath.number_of_steps = updatedPath.order_of_pages.length;
+  });
+
   return path;
 };
