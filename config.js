ba1692bbe950c5f187608fd08aafb0a3d2e09440
Adam Back <adb33@email.arizona.edu>
2016-07-29 17:31:21 -0700

    (init) Added config file for db connection.

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {
+  development: {
+    database: 'wikiDB',
+    username: 'username',
+    password: 'password',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+    port: 5432
+  }
+}
+
+module.exports = config;
