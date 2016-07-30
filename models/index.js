c9997607c937a9a65d53f6987eb64501899fc3fb
Adam Back <adb33@email.arizona.edu>
2016-07-29 17:16:22 -0700

    (init) Added sequelize models folder.

---
+++ models/index.js
@@ -0,0 +1,55 @@
+var fs        = require( 'fs');
+var path      = require( 'path' );
+var Sequelize = require( 'sequelize' );
+var basename  = path.basename( module.filename );
+var env       = process.env.NODE_ENV || 'development';
+var db        = {};
+var config    = require( '../config' )[ env ];
+var sequelize = new Sequelize( config.database, config.username, config.password, config );
+
+
+//////////////////
+// Import Schemas
+//////////////////
+
+fs.readdirSync( __dirname )
+  .filter( function( file ) {
+    return ( file.indexOf( '.' ) !== 0 ) && ( file !== basename );
+  })
+  .forEach( function( file ) {
+    var model = sequelize[ "import" ]( path.join( __dirname, file ) );
+    db[ model.name ] = model;
+  });
+
+Object.keys( db ).forEach( function( modelName ) {
+  if ( "associate" in db[ modelName ] ) {
+    db[ modelName ].associate( db );
+  }
+});
+
+//////////////////
+// Create relationships
+//////////////////
+
+// One-to-one
+
+// One-to-many
+
+// Many-to-many
+
+//////////////////
+// Sync
+//////////////////
+
+sequelize.sync()
+.then(function() {
+  console.log( 'Successfully synced database on startup.' );
+})
+.catch(function( error ) {
+  console.log( 'Error syncing database on startup:', error );
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
