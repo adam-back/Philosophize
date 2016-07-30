ac58113f2b01a60dd97668005f5eabe99272718f
Adam Back <adb33@email.arizona.edu>
2016-07-29 17:09:41 -0700

    (feat) Added basic fn to retrieve static assets from given URL.

--- helpers/wikiHelpers.js
+++ helpers/wikiHelpers.js
@@ -1,6 +1,23 @@
 var Q = require( 'q' );
 var request = require( 'request' );
+var http = {
+  request: request
+};
+
+var getWikiPage = function( url ) {
+  var deferred = Q.defer();
+
+  request( url, function ( error, response, body ) {
+    if ( !error && response.statusCode == 200 ) {
+      deferred.resolve( body );
+    } else {
+      deferred.reject( error );
+    }
+  })
+
+  return deferred.promise;
+};
 
 exports.findPhilosophy = function( url ) {
-  return Q( url );
+  return getWikiPage( url );
 };
